c68ab669ea367bc898c58abd2b97138e97b847b9
CheshireCat21 <domestardamus@gmail.com>
2021-10-28 13:47:03 +0300

    fixed view methods

--- message/urls.py
+++ message/urls.py
@@ -4,6 +4,6 @@ from message.views import MessageListView, MessageRetrieveUpdateView, MessageCre
 
 urlpatterns = [
     path('messages/list/<int:block_number>', MessageListView.as_view(), name='messages_list'),
-    path('messages/single/<int:id>', MessageRetrieveUpdateView.as_view(), name='single_message'),
+    path('messages/single/<int:pk>', MessageRetrieveUpdateView.as_view(), name='single_message'),
     path('messages/create', MessageCreateView.as_view(), name='create_message')
 ]
